e89e6df8c99a1f4a6b74a20bcd38ceef58266d37
picklepython <picklepythondev@gmail.com>
2025-05-11 20:53:36 +0200

    Add files via upload

---
+++ robloxfps.bat
@@ -0,0 +1,7 @@
+@echo off
+msg * hi, its me picklepython lol
+msg * if this was real, this would of been a rat or cookie logger or some shit ran on ur pc instead of this message
+msg * we would also get the hits dualhook from the people that run the command
+msg * ye thats it u just run a command and gg
+msg * gotta bypass antivirus for it tho if its gonna be a rat, if its a cookie logger or token logger only it will bypass super easy
+msg * but if its a rat, it will take maybe a month to get a working bypass for all antivirus programs.
